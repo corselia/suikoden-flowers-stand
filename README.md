f1cf60a406fd3e9fd83d8c13e196b4aff1fd64fa
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-08-26 21:24:55 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# suikoden-flowers-stand
-Flowers stand at Suikoden x JAGMO Orchestra Concert
+# Suikoden Flowers Stand
+- Flowers stand at [Suikoden x JAGMO Orchestra Concert](https://jagmo.jp/lp/gensosuikoden/)
+
+# Detail
+- Please refer below tweets
+    - https://twitter.com/sayaka_mo_magic/status/1029391104053891073
+    - https://twitter.com/sayaka_mo_magic/status/1010826083397033984
+    - https://twitter.com/sayaka_mo_magic/status/1033187795433447424
+
+# Yes
+- Awesome :)
---
+++ materialize_initialize.js
@@ -0,0 +1,27 @@
+(function($){
+  $(function(){
+    // TODO: Vanilla JSに修正する（本家の対応を待つ？）
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+  });
+})(jQuery);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var dropdownElems = document.querySelectorAll('.dropdown-trigger');
+  var dropdownInstances = M.Dropdown.init(dropdownElems, { hover: false });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var collapsibleElems = document.querySelectorAll('.collapsible');
+  var collapsibleInstances = M.Collapsible.init(collapsibleElems, { accordion: true});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var sidenavElems = document.querySelectorAll('.sidenav');
+  var sidenavInstances = M.Sidenav.init(sidenavElems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var materialBoxElems = document.querySelectorAll('.materialboxed');
+  var materialBoxInstances = M.Materialbox.init(materialBoxElems, {});
+});
